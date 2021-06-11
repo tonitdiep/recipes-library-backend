b6670e9d04174bdb77b1081d7cb019696ebf9927
tonitdiep <adiep123@gmail.com>
2021-06-11 19:08:11 -0400

    ran rails g scaffold

---
+++ db/migrate/20210611230520_create_recipes.rb
@@ -0,0 +1,16 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :cuisine_country
+      t.string :dietary_type
+      t.integer :time_to_cook
+      t.string :spice_level
+      t.integer :rating
+      t.integer :serving_size
+      t.string :skill_level
+
+      t.timestamps
+    end
+  end
+end
