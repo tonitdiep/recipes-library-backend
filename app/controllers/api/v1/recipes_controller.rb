058dbd99afbfb4753693d4a561b7a73d4fb083cd
tonitdiep <adiep123@gmail.com>
2021-06-14 20:24:09 -0400

    serializer updatte

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,50 +1,48 @@
 class Api::V1::RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show, :update, :destroy]
+  # before_action :set_recipe, only: [:show, :update, :destroy]
 
-  # GET /recipes
   def index
-    @recipes = Recipe.all
+    recipes = Recipe.all
 
-    render json: @recipes
+    # render json: recipes 
+    render json: RecipeSerializer.new(recipes)
   end
 
-  # GET /recipes/1
   def show
-    render json: @recipe
+    recipe = Recipe.find(params[:id])
+    # render json: recipe
+    render json: RecipeSerializer.new(recipe)
+    #RecipeSerializer
   end
 
-  # POST /recipes
   def create
-    @recipe = Recipe.new(recipe_params)
+    recipe = Recipe.new(recipe_params)
 
-    if @recipe.save
-      render json: @recipe, status: :created, location: @recipe
+    if recipe.save
+      render json: RecipeSerializer, status: :created, location: recipe
     else
-      render json: @recipe.errors, status: :unprocessable_entity
+      render json: RecipeSerializer.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /recipes/1
   def update
-    if @recipe.update(recipe_params)
-      render json: @recipe
+    if recipe.update(recipe_params)
+      render json: RecipeSerializer
     else
-      render json: @recipe.errors, status: :unprocessable_entity
+      render json: RecipeSerializer.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /recipes/1
   def destroy
-    @recipe.destroy
+    recipe.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
+    # def set_recipe
+    #   recipe = Recipe.find(params[:id])
+    # end
+
     def recipe_params
       params.require(:recipe).permit(:name, :cuisine_country, :dietary_type, :time_to_cook, :spice_level, :rating, :serving_size, :skill_level)
     end
--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,5 @@
-class RecipeSerializer < ActiveModel::Serializer
+class RecipeSerializer
+  include JSONAPI::Serializer
+  # attributes :name, :cuisine_country, :dietary_type, :spice_level, :serving_size, :skill_level
   attributes :id, :name, :dietary_type, :cuisine_country, :spice_level
 end
