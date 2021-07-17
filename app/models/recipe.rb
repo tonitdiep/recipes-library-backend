52338ea56f8adc31828652dee44722611c5c3600
tonitdiep <adiep123@gmail.com>
2021-07-16 21:19:04 -0400

    update

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
     validates :name, presence: true
     # validates :skill_level, inclusion: { in: %w(easy, medium, hard)}
+
+
+    def likeRecipeId
+        byebug
+    end
 end
