241942a5df1db0c6c19d0dea46ee6dde74f07965
tonitdiep <adiep123@gmail.com>
2021-07-21 18:32:13 -0400

    clean and update recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,5 @@
 class Recipe < ApplicationRecord
     validates :name, presence: true
-    # validates :skill_level, inclusion: { in: %w(easy, medium, hard)}
 
 
-    def likeRecipeId
-        byebug
-    end
 end
